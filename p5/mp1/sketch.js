fd490f1295922d42600b644a7953ff7b625bd0d4
Irene Govea <iigovea@ilstu.edu>
2019-09-03 01:47:18 -0500

    part one of the chaos

--- p5/mp1/sketch.js
+++ p5/mp1/sketch.js
@@ -7,7 +7,7 @@ function setup() {
 function setup() {
   // Sets the screen to be 720 pixels wide and 400 pixels high
   createCanvas(720, 400);
-  background(0);
+  background('blue');
   noStroke();
 
   fill(204);
@@ -27,4 +27,8 @@ function setup() {
 
   fill(255);
   arc(479, 300, 280, 280, PI, TWO_PI);
+}
+
+function mouseReleased() {
+  console.log(text(mouseX + ',' + mouseY,30,30)) ;
 }
