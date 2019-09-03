eb32c5ef4ff04ae868aba5c4dcb7c2c2616c6a51
Irene Govea <iigovea@ilstu.edu>
2019-09-03 04:34:53 -0500

    finished boy

--- p5/mp1/sketch.js
+++ p5/mp1/sketch.js
@@ -4,11 +4,11 @@ function setup() {
 
 }
 
-function setup() {
+//function setup() {
   // Sets the screen to be 720 pixels wide and 400 pixels high
-  createCanvas(720, 400);
-  background('blue');
-  noStroke();
+  //createCanvas(720, 400);
+  //background('blue');
+  //noStroke();
 
   //fill(204);
   //triangle(18, 18, 18, 360, 81, 360);
@@ -19,48 +19,203 @@ function setup() {
   //fill(204);
   //quad(189, 18, 216, 18, 216, 360, 144, 360);
 // these are the bold outlines of the ghostie uwu
-  fill('black');
-  ellipse(359, 185, 170, 170);
+  //fill('black');
+  //ellipse(359, 185, 170, 170);
 
-  fill('black');
-  arc(360, 370, 310, 350, PI, TWO_PI);
+  //fill('black');
+  //arc(360, 370, 310, 350, PI, TWO_PI);
 
-  fill('black');
-  ellipse(268, 360, 96, 35);
+  //fill('black');
+  //ellipse(268, 360, 96, 35);
 
-  fill('black');
-  ellipse(452, 360, 96, 35);
+  //fill('black');
+  //ellipse(452, 360, 96, 35);
 
-  fill('black');
-  ellipse(359, 360, 96, 35);
+  //fill('black');
+//  ellipse(359, 360, 96, 35);
 
 // these are the actual shapes of the ghostie uwu
 
-  fill('white');
-  ellipse(359, 185, 150, 150);
+  //fill('white');
+//  ellipse(359, 185, 150, 150);
 
-  fill('white');
-  arc(360, 350, 280, 280, PI, TWO_PI);
+//  fill('white');
+//  arc(360, 350, 280, 280, PI, TWO_PI);
 
-  fill('black');
-  ellipse(338, 176, 20, 35);
+//  fill('black');
+  //ellipse(338, 176, 20, 35);
 
-  fill('black');
-  ellipse(374, 176, 20, 35);
+//  fill('black');
+  //ellipse(374, 176, 20, 35);
 
-  fill('white');
-  ellipse(268, 352, 96, 35);
+//  fill('white');
+  //ellipse(268, 352, 96, 35);
 
-  fill('white');
-  ellipse(452, 352, 96, 35);
+  //fill('white');
+//  ellipse(452, 352, 96, 35);
 
-  fill('white');
-  ellipse(359, 349, 96, 35);
+//  fill('white');
+  //ellipse(359, 349, 96, 35);
 
   //fill(204);
 //  triangle(288, 18, 351, 360, 288, 360);
 
   //fill(255);
   //arc(376, 299, 280, 280, PI, TWO_PI);
+//}
 
-}
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background('white');
+  noStroke();
+//electric background
+
+fill('yellow');
+triangle(288, 18, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(400, 50, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(50, 60, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(200, 1, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(29, 300, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(788, 99, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(600, 300, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(455, 700, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(8, 200, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(800, 180, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(350, 5, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(700, 9, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(590, 12, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(500, 20, 351, 360, 288, 360);
+
+fill('yellow');
+triangle(60, 400, 351, 370, 288, 360);
+
+fill('yellow');
+triangle(950, 300, 370, 360, 288, 360);
+
+fill('yellow');
+triangle(1000, 600, 370, 360, 400, 360);
+
+fill('yellow');
+triangle(1500, 600, 390, 360, 350, 300);
+
+//blue spikes
+
+fill('blue');
+triangle(308, 28, 351, 360, 288, 360);
+
+fill('blue');
+triangle(550, 70, 351, 360, 288, 360);
+
+fill('blue');
+triangle(80, 90, 351, 360, 288, 360);
+
+fill('blue');
+triangle(250, 2, 351, 360, 288, 360);
+
+fill('blue');
+triangle(49, 400, 351, 360, 288, 360);
+
+fill('blue');
+triangle(988, 129, 351, 360, 288, 360);
+
+fill('blue');
+triangle(800, 700, 351, 360, 288, 360);
+
+fill('blue');
+triangle(655, 900, 351, 360, 288, 360);
+
+fill('blue');
+triangle(28, 400, 351, 360, 288, 360);
+
+fill('blue');
+triangle(1000, 380, 351, 360, 288, 360);
+
+fill('blue');
+triangle(550, 25, 351, 360, 288, 360);
+
+fill('blue');
+triangle(900, 29, 351, 360, 288, 360);
+
+fill('blue');
+triangle(790, 32, 351, 360, 288, 360);
+
+fill('blue');
+triangle(700, 40, 351, 360, 288, 360);
+
+fill('blue');
+triangle(80, 600, 351, 370, 288, 360);
+
+fill('blue');
+triangle(1150, 500, 370, 360, 288, 360);
+
+fill('blue');
+triangle(1200, 300, 370, 360, 450, 370);
+
+fill('blue');
+triangle(1700, 800, 300, 330, 350, 300);
+
+//body
+  fill('black');
+  ellipse(359, 185, 150, 150);
+
+  fill('black');
+  arc(360, 350, 280, 280, PI, TWO_PI);
+//eyes
+  fill('red');
+  ellipse(338, 176, 20, 35);
+
+  fill('red');
+  ellipse(374, 176, 20, 35);
+//pupils
+  fill('blue');
+  ellipse(374, 176, 10, 15);
+
+  fill('blue');
+  ellipse(338, 176, 10, 15);
+//body bumps uwu
+  fill('black');
+  ellipse(268, 352, 96, 35);
+
+  fill('black');
+  ellipse(452, 352, 96, 35);
+
+  fill('black');
+  ellipse(359, 349, 96, 35);
+//angry eyes
+  fill('red')
+  arc(330, 170, 30, 30, PI, TWO_PI);
+
+  fill('red')
+  arc(380, 170, 30, 30, PI, TWO_PI);
+//mouth
+  fill('red')
+  arc(356, 220, 30, 30, PI, TWO_PI);
+
+  }
