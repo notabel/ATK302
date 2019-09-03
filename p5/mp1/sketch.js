e8d499126d1f820819a284a54ae9d73ee7fd095d
Irene Govea <iigovea@ilstu.edu>
2019-09-03 02:43:47 -0500

    FINISHED GHOSTIE

--- p5/mp1/sketch.js
+++ p5/mp1/sketch.js
@@ -10,27 +10,57 @@ function setup() {
   background('blue');
   noStroke();
 
-  fill(204);
-  triangle(18, 18, 18, 360, 81, 360);
+  //fill(204);
+  //triangle(18, 18, 18, 360, 81, 360);
 
-  fill(102);
-  rect(81, 81, 63, 63);
+  //fill(102);
+  //rect(81, 81, 63, 63);
 
-  fill(204);
-  quad(189, 18, 216, 18, 216, 360, 144, 360);
+  //fill(204);
+  //quad(189, 18, 216, 18, 216, 360, 144, 360);
+// these are the bold outlines of the ghostie uwu
+  fill('black');
+  ellipse(359, 185, 170, 170);
 
-  fill(255);
-  ellipse(252, 144, 72, 72);
+  fill('black');
+  arc(360, 370, 310, 350, PI, TWO_PI);
 
-  fill(204);
-  triangle(288, 18, 351, 360, 288, 360);
+  fill('black');
+  ellipse(268, 360, 96, 35);
 
-  fill(255);
-  arc(479, 300, 280, 280, PI, TWO_PI);
-  fill(255) ;
-  text(mouseX + ',' + mouseY,30,30)
-}
+  fill('black');
+  ellipse(452, 360, 96, 35);
 
-function mouseReleased() {
-  console.log(text(mouseX + ',' + mouseY,30,30)) ;
+  fill('black');
+  ellipse(359, 360, 96, 35);
+
+// these are the actual shapes of the ghostie uwu
+
+  fill('white');
+  ellipse(359, 185, 150, 150);
+
+  fill('white');
+  arc(360, 350, 280, 280, PI, TWO_PI);
+
+  fill('black');
+  ellipse(338, 176, 20, 35);
+
+  fill('black');
+  ellipse(374, 176, 20, 35);
+
+  fill('white');
+  ellipse(268, 352, 96, 35);
+
+  fill('white');
+  ellipse(452, 352, 96, 35);
+
+  fill('white');
+  ellipse(359, 349, 96, 35);
+
+  //fill(204);
+//  triangle(288, 18, 351, 360, 288, 360);
+
+  //fill(255);
+  //arc(376, 299, 280, 280, PI, TWO_PI);
+
 }
