9d32f27c87d4e0ffa157a7661fd8f507b2d89f6f
Irene Govea <iigovea@ilstu.edu>
2019-09-05 10:06:18 -0500

    hope this works pt 2

--- p5/mp1/sketch.js
+++ p5/mp1/sketch.js
@@ -4,66 +4,6 @@ function setup() {
 
 }
 
-//function setup() {
-  // Sets the screen to be 720 pixels wide and 400 pixels high
-  //createCanvas(720, 400);
-  //background('blue');
-  //noStroke();
-
-  //fill(204);
-  //triangle(18, 18, 18, 360, 81, 360);
-
-  //fill(102);
-  //rect(81, 81, 63, 63);
-
-  //fill(204);
-  //quad(189, 18, 216, 18, 216, 360, 144, 360);
-// these are the bold outlines of the ghostie uwu
-  //fill('black');
-  //ellipse(359, 185, 170, 170);
-
-  //fill('black');
-  //arc(360, 370, 310, 350, PI, TWO_PI);
-
-  //fill('black');
-  //ellipse(268, 360, 96, 35);
-
-  //fill('black');
-  //ellipse(452, 360, 96, 35);
-
-  //fill('black');
-//  ellipse(359, 360, 96, 35);
-
-// these are the actual shapes of the ghostie uwu
-
-  //fill('white');
-//  ellipse(359, 185, 150, 150);
-
-//  fill('white');
-//  arc(360, 350, 280, 280, PI, TWO_PI);
-
-//  fill('black');
-  //ellipse(338, 176, 20, 35);
-
-//  fill('black');
-  //ellipse(374, 176, 20, 35);
-
-//  fill('white');
-  //ellipse(268, 352, 96, 35);
-
-  //fill('white');
-//  ellipse(452, 352, 96, 35);
-
-//  fill('white');
-  //ellipse(359, 349, 96, 35);
-
-  //fill(204);
-//  triangle(288, 18, 351, 360, 288, 360);
-
-  //fill(255);
-  //arc(376, 299, 280, 280, PI, TWO_PI);
-//}
-
 function draw() {
   // Sets the screen to be 720 pixels wide and 400 pixels high
   createCanvas(720, 400);
@@ -219,6 +159,9 @@ triangle(1700, 800, 300, 330, 350, 300);
 //mouth
   fill('red')
   arc(356, 220, 30, 30, PI, TWO_PI);
+
+  fill(0)
+  text('This is Dr. Ghostie. He is a ghost with a PhD in criminal investigation for the dead. Thanks to his deathliness, he can sneak in and out of restricted areas he ventures into when searching for clues and solving a mystery. Unfortunately, he can never solve any of his cases, all for the same reason he cannot remember...', mouseX, mouseY)
 } else {
   background('blue');
   noStroke();
