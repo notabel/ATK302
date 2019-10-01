3da810f48c0439681d07e015a1ae573d18323280
Irene Govea <iigovea@ilstu.edu>
2019-09-30 21:40:00 -0500

    bouncy boy

--- p5/week6multsound/sketch.js
+++ p5/week6multsound/sketch.js
@@ -1,7 +1,31 @@
+let mic;
+
 function setup() {
-  // put setup code here
+  createCanvas(710, 200);
+
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+
+  // Get the overall volume (between 0 and 1.0)
+  let vol = mic.getLevel();
+  fill('yellow');
+  stroke(0);
+
+  // Draw an ellipse with height based on volume
+  let h = map(vol, 0, .1, height, 0);
+    let c = map(vol, 0 , .1, 0, 500);
+    fill(c) ;
+      background (c, 0 ,0);
+  ellipse(width / 2, h - 25, 50, 200);
+}
+
+function touchStarted() {
+    getAudioContext().resume();
 }
