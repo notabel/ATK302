a93ecb1dbe9ce001a76f4d7827fcb7d7c9ca2b64
Irene Govea <iigovea@ilstu.edu>
2019-10-01 14:02:48 -0500

    h

--- p5/week6stop1/sketch.js
+++ p5/week6stop1/sketch.js
@@ -24,7 +24,7 @@ function draw() {
       fill('green');
       ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
 
-      doTimer(1, 100);
+      doTimer(1, 200);
 
       break;
 
@@ -37,7 +37,7 @@ function draw() {
       fill('yellow');
       ellipse(width / 2, height / 2, 100, 100); // middle
 
-      doTimer(2, 200);
+      doTimer(2, 100);
 
       break;
 
