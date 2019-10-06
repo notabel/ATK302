0f35a48907f8d87737deff17c7a42435b9cf2521
Irene Govea <iigovea@ilstu.edu>
2019-10-05 19:58:42 -0500

    c

--- p5/mp2/sketch.js
+++ p5/mp2/sketch.js
@@ -30,7 +30,7 @@ function draw() {
       image(scenePic, 20, 100);
       fill('red');
       text("Left click to begin viewing portfolio and to skip ahead. Slides change on their own. Speak to make the circle cheer.", 10, height / 3);
-      textSize(12);
+      textSize(13);
 
       break;
 
