8bfbac18bf700ed58abe337a27e80b518ba390ad
Irene Govea <iigovea@ilstu.edu>
2019-11-19 15:00:52 -0600

    gxfhgjhkjk

--- p5/mp4project/sketch.js
+++ p5/mp4project/sketch.js
@@ -1,14 +1,22 @@
 var peppa;
+var trochut;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   peppa = loadImage("assets/pepper.jpg");
+  myF = loadFont("assets/fontss/Trochut-Regular.ttf");
 }
 
 function draw() {
   background("white");
+  push();
   rotateZ(radians(rotationZ));
-  text(rotationZ, width/2, height/2);
+  box(200, 200, 200);
+
+  pop();
+  fill("black");
+  text(rotationZ, width / 2, height / 2);
+
   //rotateX(radians(rotationX));
   //rotateY(radians(rotationY));
 }
