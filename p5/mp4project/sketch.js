8c83495f571b86db25f8e1a2b174cb37be926159
Irene Govea <iigovea@ilstu.edu>
2019-11-19 14:07:35 -0600

    what do

--- p5/mp4project/sketch.js
+++ p5/mp4project/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  // put setup code here
+  createCanvas(100, 100, WEBGL);
 }
 
 function draw() {
-  // put drawing code here
+  background(200);
+  rotateZ(radians(rotationZ));
+  //rotateX(radians(rotationX));
+  //rotateY(radians(rotationY));
+  box(200, 200, 200);
 }
