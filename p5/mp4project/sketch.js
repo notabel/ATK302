a1acba56d7d37162b44f50ab362f1df14e112691
Irene Govea <iigovea@ilstu.edu>
2019-11-21 14:04:07 -0600

    asrtyuj

--- p5/mp4project/sketch.js
+++ p5/mp4project/sketch.js
@@ -1,3 +1,11 @@
+var alpha, beta, gamma; // orientation data
+var xPosition = 0;
+var yPosition = 0;
+var x = 0; // acceleration data
+var y = 0;
+var z = 0;
+var frogPos;
+
 var peppa;
 var trochut;
 
@@ -5,10 +13,24 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   peppa = loadImage("assets/pepper.jpg");
   myF = loadFont("assets/fontss/Trochut-Regular.ttf");
+
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+
+
+  frogPos = createVector(width / 2, height - 80);
+
+
+  frogPos = createVector(width / 2, height - 80);
+
+  imageMode(CENTER);
+  rectMode(CENTER);
+
 }
 
 function draw() {
-  background("white");
+  background("yellow");
   push();
   rotateZ(radians(rotationZ));
   fill("red");
@@ -16,8 +38,67 @@ function draw() {
 
   pop();
   fill("white");
-  text("yeet",rotationZ, width / 2, height / 2);
+  text("communism",rotationZ, width / 2, height / 2);
 
   //rotateX(radians(rotationX));
   //rotateY(radians(rotationY));
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
+
+  push(); // before you use translate, rotate, or scale commands, push and then pop after
+
+  translate(xPosition, yPosition); // move everything over by x, y
+
+  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
+
+  image(peppa, 0, 0, 500, 500);
+
+  frogPos.x = xPosition;
+  frogPos.y = yPosition;
+
+  
+
+  // DECORATIONS
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);
+
+  // MORE DECORATIONS - write that pretty ATK type on top.
+  fill('white');
+  noStroke();
+  textSize(300);
+  textAlign(CENTER);
+  text("atk", width / 2, height / 2);
+
 }
+
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});
+''
